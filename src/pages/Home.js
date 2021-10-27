95d5be3ff6bb8553a0f54e1be13ec9a979c44b82
unknown <braj.singh@truemeds.in>
2021-10-27 16:40:01 +0530

    some changes are done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import Services from "../components/Services";
 import FeaturedRooms from "../components/FeaturedRooms";
+// import FeaturedRooms from "../components/FeaturedRooms";
 const home = () => {
   return (
     <>
